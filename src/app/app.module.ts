ad881f9cd114da3e8a1de30732a02bfc5e9c6f3a
Mateus Sachetti <50852444+masachetti@users.noreply.github.com>
2024-04-01 20:49:41 -0400

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NovaTransacaoComponent } from './nova-transacao/nova-transacao.component';
+import { TransacoesComponent } from './transacoes/transacoes.component';
+import { ButtonComponent } from './button/button.component';
+import { TransactionsTableComponent } from './transactions-table/transactions-table.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { NgIconsModule } from '@ng-icons/core';
+import { matChevronLeft as MatChevronLeft, matChevronRight as MatChevronRight } from '@ng-icons/material-icons/baseline'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransacaoComponent,
+    TransacoesComponent,
+    ButtonComponent,
+    TransactionsTableComponent,
+    PaginationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgIconsModule.withIcons({ MatChevronLeft, MatChevronRight }),
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
