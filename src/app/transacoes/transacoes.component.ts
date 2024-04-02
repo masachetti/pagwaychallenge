5b2977821e030cc5a9633e25fe0056aa38d0ef9b
Mateus Sachetti <50852444+masachetti@users.noreply.github.com>
2024-04-01 20:50:04 -0400

    Develop transactions page

--- src/app/transacoes/transacoes.component.ts
+++ src/app/transacoes/transacoes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionService } from '../transaction.service';
+import { ActivatedRoute } from '@angular/router';
+import { PaginateInfo } from 'src/types/paginated';
+import { Transaction } from 'src/types/transaction';
 
 @Component({
   selector: 'app-transacoes',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transacoes.component.css']
 })
 export class TransacoesComponent implements OnInit {
+  page: number;
+  perPage: number;
+  paginateInfo: PaginateInfo | undefined
+  transactions: Array<Transaction> | undefined
 
-  constructor() { }
+
+  constructor(private transaction: TransactionService, private route: ActivatedRoute) {
+    this.page = 1
+    this.perPage = 10
+  }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.page = params['page'] ?? this.page
+      this.perPage = params['per_page'] ?? this.perPage
+      this.transaction.getTransactions(this.page, this.perPage).subscribe((data) => {
+        const { data: _data, ..._paginateInfo } = data
+        this.transactions = _data;
+        this.paginateInfo = _paginateInfo;
+      })
+    })
   }
 
 }
