b4ea578416b9c59d2f7e7ee9da8aa970681c7dd4
Mateus Sachetti <mateus.sachetti@gmail.com>
2024-04-02 17:10:14 -0400

    Fix query params forwarding as string instead of numbers

--- src/app/transacoes/transacoes.component.ts
+++ src/app/transacoes/transacoes.component.ts
@@ -7,30 +7,33 @@ import { Transaction } from 'src/types/transaction';
 @Component({
   selector: 'app-transacoes',
   templateUrl: './transacoes.component.html',
-  styleUrls: ['./transacoes.component.css']
+  styleUrls: ['./transacoes.component.css'],
 })
 export class TransacoesComponent implements OnInit {
   page: number;
   perPage: number;
-  paginateInfo: PaginateInfo | undefined
-  transactions: Array<Transaction> | undefined
+  paginateInfo: PaginateInfo | undefined;
+  transactions: Array<Transaction> | undefined;
 
-
-  constructor(private transaction: TransactionService, private route: ActivatedRoute) {
-    this.page = 1
-    this.perPage = 10
+  constructor(
+    private transaction: TransactionService,
+    private route: ActivatedRoute
+  ) {
+    this.page = 1;
+    this.perPage = 10;
   }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.page = params['page'] ?? this.page
-      this.perPage = params['per_page'] ?? this.perPage
-      this.transaction.getTransactions(this.page, this.perPage).subscribe((data) => {
-        const { data: _data, ..._paginateInfo } = data
-        this.transactions = _data;
-        this.paginateInfo = _paginateInfo;
-      })
-    })
+    this.route.queryParams.subscribe((params) => {
+      this.page = parseInt(params['page'] ?? this.page);
+      this.perPage = parseInt(params['per_page'] ?? this.perPage);
+      this.transaction
+        .getTransactions(this.page, this.perPage)
+        .subscribe((data) => {
+          const { data: _data, ..._paginateInfo } = data;
+          this.transactions = _data;
+          this.paginateInfo = _paginateInfo;
+        });
+    });
   }
-
 }
