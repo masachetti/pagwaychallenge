45da9e27c12cd83056e275e1f307c730f388d572
Mateus Sachetti <50852444+masachetti@users.noreply.github.com>
2024-04-01 20:48:23 -0400

    Replace twmerge with the CS utility class on button component

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,28 +1,18 @@
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import { clsx as clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { CS } from 'src/utils/cs';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent extends CS implements OnInit {
   @Input() class = '';
 
   constructor() {
+    super();
   }
 
   ngOnInit(): void {
   }
 
-  cs(...classes: string[]) {
-    console.log(classes)
-    const ret = twMerge(clsx(classes))
-    console.log(ret)
-    return ret
-  }
-
-  ola() {
-    console.log('Ola mundo')
-  }
 }
