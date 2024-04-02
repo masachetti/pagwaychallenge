bcbf83a4790747eb840b7660b1223e30fd40e51a
Mateus Sachetti <mateus.sachetti@gmail.com>
2024-04-02 12:32:55 -0400

    Improve pagination component

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,24 +1,25 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PaginateInfo } from 'src/types/paginated';
+import { CS } from 'src/utils/cs';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css'],
 })
-export class PaginationComponent implements OnChanges {
-
+export class PaginationComponent extends CS implements OnChanges {
   @Input() paginateInfo: PaginateInfo | undefined;
 
   pages: Array<number> | undefined;
 
-  constructor() { }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const info = changes['paginateInfo'].currentValue
-    if (info) {
-      this.pages = Array.from({ length: info.pages }, (_, i) => i + 1)
-    }
+  constructor() {
+    super();
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    const info = changes['paginateInfo'].currentValue;
+    if (info) {
+      this.pages = Array.from({ length: info.pages }, (_, i) => i + 1);
+    }
+  }
 }
