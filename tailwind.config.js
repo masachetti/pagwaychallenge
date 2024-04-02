0f2de3901cfbc79136ee8028d5883d7a451d1792
Mateus Sachetti <50852444+masachetti@users.noreply.github.com>
2024-04-01 20:42:24 -0400

    Change the method to enable darkmode on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
