d0a4264d8b6a29c53dc32b535f4ebed4c72ad3c3
Mateus Sachetti <mateus.sachetti@gmail.com>
2024-04-12 17:00:52 -0400

    Change mocked data

---
+++ db.js
@@ -0,0 +1,55 @@
+const casual = require("casual");
+const fs = require('fs')
+
+casual.define("recebivel", function (value) {
+  const possuiRecebivel = casual.coin_flip;
+  let statusRecebivel = null;
+  let dataPagamentoRecebivel = null;
+  let valorRecebivel = null;
+  if (possuiRecebivel) {
+    statusRecebivel = casual.random_element(["Pendente", "Pago"]);
+    const deltaTimeStamp = casual.integer((from = 0), (to = 31536000));
+    dataPagamentoRecebivel = ~~(
+      new Date().getTime() / 1000 +
+      (statusRecebivel === "Pendente" ? deltaTimeStamp : -deltaTimeStamp)
+    );
+
+    valorRecebivel = ~~(value * 0.05);
+  }
+  return {
+    possuiRecebivel: possuiRecebivel,
+    statusRecebivel: statusRecebivel,
+    dataPagamentoRecebivel: dataPagamentoRecebivel,
+    valorRecebivel: valorRecebivel,
+  };
+});
+
+casual.define("transacao", function () {
+  const value = casual.integer((from = 1000), (to = 99999)) * 100;
+  const recebivel = casual.recebivel(value);
+  const deltaTimeStamp = casual.integer((from = 0), (to = 31536000));
+  const dataTransacao = ~~(new Date().getTime() / 1000 - deltaTimeStamp);
+  return {
+    valor: value,
+    descricao: casual.short_description,
+    nomePortadorCartao: casual.full_name,
+    numeroCartao: casual.card_number(),
+    validadeCartao: casual.card_exp,
+    codigoSegurancaCartao: casual.integer((to = 100), (from = 999)),
+    dataTransacao: dataTransacao,
+    ...recebivel,
+  };
+});
+
+const transacoes = {
+    transacoes: []
+}
+for (let i = 1; i<=500 ; i++){
+    transacoes.transacoes.push({
+        ...casual.transacao,
+        id: i
+    })
+}
+const result = JSON.stringify(transacoes)
+
+fs.writeFile('db3.json', result, (err) => console.log(err))
